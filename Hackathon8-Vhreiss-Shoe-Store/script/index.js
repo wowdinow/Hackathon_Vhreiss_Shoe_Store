baac33cd36595b85be7cca1dcae9f09388e96390
Frando <111839019+wowdinow@users.noreply.github.com>
2023-08-02 17:16:02 +0700

    Add files via upload

---
+++ Hackathon8-Vhreiss-Shoe-Store/script/index.js
@@ -0,0 +1,86 @@
+const listProduct = [
+    {
+        merk : "Adudu",
+        name: "Adudu Classic",
+        price: 1_500_000,
+        image:""
+    },
+    {
+        merk : "Niko",
+        name: "Niko Vintage Pink",
+        price: 1_000_000,
+        image:""
+    },
+    {
+        merk : "Pumo",
+        name: "Pumo Run 2030",
+        price: 2_500_000,
+        image:""
+    },
+    {
+        merk : "Adudu",
+        name: "Adudu Run Free White",
+        price: 2_000_000,
+        image:""
+    },
+    {
+        merk : "Adudu",
+        name: "Adudu Boost 99.0",
+        price: 5_000_000,
+        image:""
+    },
+    {
+        merk : "Adudu",
+        name: "Adudu Super Boost Black",
+        price: 3_500_000,
+        image:""
+    },
+    {
+        merk : "Pumo",
+        name: "Pumo Vintage Blue",
+        price: 1_000_000,
+        image:""
+    },
+    {
+        merk : "Pumo",
+        name: "Pumo Simple Sneakers",
+        price: 750_000,
+        image:""
+    },
+    {
+        merk : "Niko",
+        name: "Niko Runner Red",
+        price: 3_000_000,
+        image:""
+    },
+    {
+        merk : "Niko",
+        name: "Niko Runner Wow",
+        price: 2_000_000,
+        image:""
+    },
+    {
+        merk : "Niko",
+        name: "Niko Gym Uye",
+        price: 800_000,
+        image:""
+    },
+    {
+        merk : "Niko",
+        name: "Niko Beach Runner",
+        price: 1_500_000,
+        image:""
+    },
+    {
+        merk : "Niko",
+        name: "Niko Green Goblin",
+        price: 2_000_000,
+        image:""
+    },
+    {
+        merk : "Niko",
+        name: "Niko Matcha Runner",
+        price: 2_000_000,
+        image:""
+    },
+]
